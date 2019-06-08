593b5822614afcb82f9669766f9c9eb1035b928e
Geoff Thomas <admiralgt@gmail.com>
2019-06-09 00:36:11 +0100

    Add some comments because I'm not a cowboy

--- Event.py
+++ Event.py
@@ -4,34 +4,53 @@ import logging
 logger = logging.getLogger('gop_scorer')
 
 class Event(object):
+    """
+    An event class, u
+    """
 
     def __init__(self, teams, scores):
         self.scores = scores
         self.teams = teams
 
-    def total_strength(self, teams):
+    def total_strength(self):
+        """
+        The total strength of all remaining teams in this event
+        :return: The sum of the teams strength
+        """
         total_strength = 0
-        for team in teams:
+        for team in self.teams:
             total_strength += team.strength
         return total_strength
 
-    def winning_team(self, teams, strength):
+    def choose_next_placed_team(self, strength):
+        """
+        Choose the next placed team in this event
+        :param strength: The strength (between 0 and the combined teams strength) to choose the team
+        :return: Team object, that placed next highest
+        """
         current_strength = 0
         winning_team = None
-        for team in teams:
+        for team in self.teams:
             current_strength += team.strength
             if strength <= current_strength:
                 return team
 
     def score_event(self):
-        teams = list(self.teams)
+        """
+        Score the event.
+        :return: None
+        """
+
         for ii in range(len(self.teams)):
             logger.debug('Scoring position {}'.format(ii + 1))
-            total_strength = self.total_strength(teams)
+            # Determine the total strength of all remaining teams
+            total_strength = self.total_strength()
             logger.debug('Total strength {}'.format(total_strength))
             winning_strength = random.uniform(0, total_strength)
             logger.debug('Winning strength {}'.format(winning_strength))
-            winning_team = self.winning_team(teams, winning_strength)
-            logger.debug('Winning team {}'.format(winning_team.name))
-            winning_team.score_points(self.scores[ii])
-            teams.remove(winning_team)
+            # Choose which team places next
+            next_placed_team = self.choose_next_placed_team(winning_strength)
+            logger.debug('Winning team {}'.format(next_placed_team.name))
+            next_placed_team.score_points(self.scores[ii])
+            # Remove this team from the remaining teams
+            self.teams.remove(next_placed_team)
--- Participant.py
+++ Participant.py
@@ -3,14 +3,31 @@ import logging
 logger = logging.getLogger('gop_scorer')
 
 class EventParticipant(object):
+    """
+    Super class of Participants
+    """
 
     def __init__(self, name, strength):
+        """
+
+        :param name: The name of this participant
+        :param strength: The strength of this participant
+        """
         self.name = name
         self.strength = float(strength)
 
 class Team(EventParticipant):
+    """
+    A Team of players e.g. Stark, Lannister
+    """
 
     def __init__(self, name, alliance, strength):
+        """
+
+        :param name: The name of this team, used to identify team
+        :param alliance: The name of the alliance this team is in.
+        :param strength: The comparative strength of this team.
+        """
         super(Team, self).__init__(name, strength)
         self.alliance = alliance
         self.points = 0
@@ -18,28 +35,51 @@ class Team(EventParticipant):
         self.total_points = 0
 
     def score_points(self, score):
+        """
+        Score points for this team for an event.
+        :param score: The score this team received in an event
+        """
         logger.debug('Team {} scoring {} points'.format(self.name, score))
         self.points += int(score)
         logger.debug('Team {} now has {} points'.format(self.name, self.points))
 
     def record_result(self, position):
+        """
+        Record a result for a competition
+        :param position: The position the team finished in.
+        """
         logger.debug('Team {} scored {} points in position {}'.format(self.name, self.points, position))
         self.total_points += self.points
         self.total_positions += position
         self.points = 0
 
 class Alliance(EventParticipant):
+    """
+    An alliance of teams.
+    """
 
     def __init__(self, name):
+        """
+
+        :param name: The name of the alliance
+        """
         super(Alliance, self).__init__(name, 0)
         self.teams = []
 
     def addTeam(self, team):
+        """
+        Add a team to this alliance
+        :param team: A Team object to add to this alliance
+        """
         logger.info('Added {} to alliance {}'.format(team.name, self.name))
         self.teams.append(team)
         self.strength += team.strength
 
     def score_points(self, score):
+        """
+        Score points for all the teams in this alliance for an Alliance event.
+        :param score: The score this alliance got
+        """
         logger.debug('Alliance {} scoring {} points'.format(self.name, score))
         for team in self.teams:
             team.score_points(score)
